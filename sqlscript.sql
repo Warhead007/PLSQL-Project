a0441ffc0afb9d510666e5f299961a2e206ea9f1
Pekky29 <35894246+Pekky29@users.noreply.github.com>
2019-05-14 13:09:40 +0700

    Update Show Score Chapter 1 - 3

--- sqlscript.sql
+++ sqlscript.sql
@@ -1,57 +1,33 @@
-SET serveroutput ON;
-
-CREATE OR REPLACE  PROCEDURE ANALYZE_TEST(p_registrationno registration.registrationno%TYPE) IS
+create or replace PROCEDURE ANALYZE_TEST(p_subcode registration.subjectcode%TYPE) IS
     v_correct_count NUMBER := 0;
+    v_correct_count_chap NUMBER := 0;
     v_maxscore NUMBER := 0;
-    v_regisnum NUMBER := p_registrationno;
-    v_sub registration.subjectcode%TYPE;
+    v_maxscorechap NUMBER := 0;
+    v_regissub VARCHAR2(20) := p_subcode;
     v_ans NUMBER := 0;
     v_test NUMBER := 0;
-    v_ans_text answerbank.answer%TYPE;
-    v_test_text answerbank.answer%TYPE;
-    v_a_text answerbank.answer%TYPE;
-    v_b_text answerbank.answer%TYPE;
-    v_c_text answerbank.answer%TYPE;
-    v_d_text answerbank.answer%TYPE;
-    CURSOR regisid_cur (p_registrationno_cur registration.registrationno%TYPE) IS
-        SELECT r.subjectcode,r.testdate,qt.a_id,qt.b_id,qt.c_id,qt.d_id,qt.answer,
-        qt.testanswer,qb.chapter,qt.questionid,qb.question,qt.registrationno
+    v_chapter NUMBER := 1;
+    CURSOR regissub_cur (p_subcode registration.subjectcode%TYPE) IS
+        SELECT *
         FROM questiontest qt
-        JOIN questionbank qb ON qt.questionid = qb.questionid
-        JOIN registration r ON r.registrationno = qt.registrationno
-        WHERE qt.registrationno = p_registrationno;
-    regisid_rec regisid_cur%ROWTYPE;
-    
-    v_other_ques NUMBER := 0;
-    v_true_count NUMBER := 0;
-    v_false_count NUMBER := 0;
-    v_regis_id NUMBER := 0;
-    v_a_id NUMBER := 0;
-    v_b_id NUMBER := 0;
-    v_c_id NUMBER := 0;
-    v_d_id NUMBER := 0;
-    v_a_count NUMBER := 0;
-    v_b_count NUMBER := 0;
-    v_c_count NUMBER := 0;
-    v_d_count NUMBER := 0;
-    CURSOR other_regis_cur IS
-        SELECT r.subjectcode,r.testdate,qt.a_id,qt.b_id,qt.c_id,qt.d_id,qt.answer,qt.testanswer,qt.questionid,qb.question
-        FROM registration r
-        JOIN questiontest qt ON r.registrationno = qt.registrationno
-        JOIN questionbank qb ON qt.questionid = qb.questionid;
-    
-BEGIN
-    SELECT subjectcode INTO v_sub
-    FROM registration
-    WHERE registrationno = p_registrationno;
-    OPEN regisid_cur(p_registrationno);
-    DBMS_OUTPUT.PUT_LINE('Registation ID: ' || v_regisnum);
-    DBMS_OUTPUT.PUT_LINE('Subject: ' || v_sub);
-    DBMS_OUTPUT.PUT_LINE('');
-    LOOP
-        FETCH regisid_cur INTO regisid_rec;
-        EXIT WHEN regisid_cur%NOTFOUND;
-        ----Analysis answer----
+        JOIN questionbank qb on qt.questionid = qb.questionid
+        WHERE qb.subjectcode = p_subcode
+        ORDER BY qb.chapter ASC;
+   CURSOR maxscorechap_cur (p_subcode registration.subjectcode%TYPE) IS
+        SELECT *
+        FROM questiontest qt
+        JOIN questionbank qb on qt.questionid = qb.questionid
+        WHERE qb.subjectcode = p_subcode and qb.chapter = v_chapter;
+    regisid_rec regissub_cur%ROWTYPE;    
+    maxscorechap_rec maxscorechap_cur%ROWTYPE;
+    BEGIN
+        OPEN regissub_cur(p_subcode);
+        DBMS_OUTPUT.PUT_LINE('Subject : ' || v_regissub);
+        DBMS_OUTPUT.PUT_LINE('');
+        LOOP
+        FETCH regissub_cur INTO regisid_rec;
+        EXIT WHEN regissub_cur%NOTFOUND;
+    ----Analysis answer----
         IF regisid_rec.answer = 'A' THEN
             v_ans := regisid_rec.a_id;
             CASE
@@ -62,7 +38,7 @@ BEGIN
                 WHEN regisid_rec.testanswer = 'C' THEN
                     v_test := regisid_rec.c_id;
                 ELSE
-                    v_test := regisid_rec.d_id;
+                    v_test := regisid_rec.c_id;
             END CASE;
         ELSIF regisid_rec.answer = 'B' THEN
             v_ans := regisid_rec.b_id;
@@ -74,7 +50,7 @@ BEGIN
                 WHEN regisid_rec.testanswer = 'C' THEN
                     v_test := regisid_rec.c_id;
                 ELSE
-                    v_test := regisid_rec.d_id;
+                    v_test := regisid_rec.c_id;
             END CASE;
         ELSIF regisid_rec.answer = 'C' THEN
             v_ans := regisid_rec.c_id;
@@ -86,7 +62,7 @@ BEGIN
                 WHEN regisid_rec.testanswer = 'C' THEN
                     v_test := regisid_rec.c_id;
                 ELSE
-                    v_test := regisid_rec.d_id;
+                    v_test := regisid_rec.c_id;
             END CASE;
         ELSE
             v_ans := regisid_rec.d_id;
@@ -98,128 +74,102 @@ BEGIN
                 WHEN regisid_rec.testanswer = 'C' THEN
                     v_test := regisid_rec.c_id;
                 ELSE
-                    v_test := regisid_rec.d_id;
+                    v_test := regisid_rec.c_id;
             END CASE;
         END IF;
-            
-        v_true_count := 0;
-        v_false_count := 0;
-            
-        v_a_count := 0;
-        v_b_count := 0;
-        v_c_count := 0;
-        v_d_count := 0;
-            
-        FOR other_regis_rec IN other_regis_cur LOOP
-            IF regisid_rec.subjectcode = other_regis_rec.subjectcode
-            AND regisid_rec.testdate = other_regis_rec.testdate
-            AND regisid_rec.questionid = other_regis_rec.questionid THEN
-            v_a_id := regisid_rec.a_id;
-            v_b_id := regisid_rec.b_id;
-            v_c_id := regisid_rec.c_id;
-            v_d_id := regisid_rec.d_id;
-            CASE
-                WHEN other_regis_rec.testanswer = 'A' THEN
-                    v_regis_id := other_regis_rec.a_id;
-                WHEN other_regis_rec.testanswer = 'B' THEN
-                    v_regis_id := other_regis_rec.b_id;    
-                WHEN other_regis_rec.testanswer = 'C' THEN
-                    v_regis_id := other_regis_rec.c_id;
-                ELSE
-                    v_regis_id := other_regis_rec.d_id;
-            END CASE;
-            CASE
-                WHEN v_regis_id = v_a_id THEN
-                    v_a_count := v_a_count + 1;
-                WHEN v_regis_id = v_b_id THEN
-                    v_b_count := v_b_count + 1;
-                WHEN v_regis_id = v_c_id THEN
-                    v_c_count := v_c_count + 1;
-                ELSE
-                    v_d_count := v_d_count + 1;
-                END CASE;
-            IF v_regis_id = v_ans THEN
-                v_true_count := v_true_count + 1;
-            ELSIF  v_regis_id != v_ans THEN
-                v_false_count := v_false_count + 1;
-            END IF;
-                v_other_ques := other_regis_rec.questionid;
-            END IF;
-        END LOOP;
-            
-        SELECT answer INTO v_ans_text
-        FROM answerbank
-        WHERE answerid = v_ans;
-        SELECT answer INTO v_test_text
-        FROM answerbank
-        WHERE answerid = v_test;
-            
-        SELECT answer INTO v_a_text
-        FROM answerbank
-        WHERE answerid = v_a_id;
-        SELECT answer INTO v_b_text
-        FROM answerbank
-        WHERE answerid = v_b_id;
-        SELECT answer INTO v_c_text
-        FROM answerbank
-        WHERE answerid = v_c_id;
-        SELECT answer INTO v_d_text
-        FROM answerbank
-        WHERE answerid = v_d_id;
-        DBMS_OUTPUT.PUT_LINE('');
-        DBMS_OUTPUT.PUT_LINE('Chapter: ' || regisid_rec.chapter);
         DBMS_OUTPUT.PUT_LINE('Question: ' || regisid_rec.question);
-        DBMS_OUTPUT.PUT_LINE('Correct answer: ' || v_ans_text);
-        DBMS_OUTPUT.PUT_LINE('User answer: ' || v_test_text);
+        DBMS_OUTPUT.PUT_LINE('Subject Code: ' || regisid_rec.subjectcode || ' Chapter: ' || regisid_rec.chapter 
+                             || ' Correct answer ID: ' || v_ans || ' User answer ID: ' || v_test);
         ----Count score----
         IF regisid_rec.answer = regisid_rec.testanswer THEN 
             v_correct_count := v_correct_count + 1;
-            DBMS_OUTPUT.PUT_LINE('User answer is correct');
+            DBMS_OUTPUT.PUT_LINE('True!');
         ELSE
-            DBMS_OUTPUT.PUT_LINE('User answer is uncorrect');
+            DBMS_OUTPUT.PUT_LINE('False!');
         END IF;
         v_maxscore := v_maxscore + 1;
-        DBMS_OUTPUT.PUT_LINE('-----All users result------');
-        DBMS_OUTPUT.PUT_LINE('All users answer correct: ' || v_true_count);
-        DBMS_OUTPUT.PUT_LINE('All users answer uncorrect: ' || v_false_count);
-        DBMS_OUTPUT.PUT_LINE('-----All users choose------');
-        SORT_ANSWER(v_a_text,v_b_text,v_c_text,v_d_text,v_a_count,v_b_count,v_c_count,v_d_count);
-    END LOOP;
-    DBMS_OUTPUT.PUT_LINE('');
-    DBMS_OUTPUT.PUT_LINE('Score of ' || regisid_rec.registrationno || ' is ' || v_correct_count || ' / ' ||  v_maxscore);
-    CLOSE regisid_cur;
-END ANALYZE_TEST;
+        DBMS_OUTPUT.PUT_LINE('');
+        
+        
+        DBMS_OUTPUT.PUT_LINE('');
+        END LOOP;
 
-CREATE TYPE answer_obj IS OBJECT(
-    count_value NUMBER,
-    text_value VARCHAR2(4000));
-/
-CREATE TYPE answer_table IS TABLE OF answer_obj;
-/
-CREATE OR REPLACE  PROCEDURE SORT_ANSWER (p_a_text answerbank.answer%TYPE,p_b_text answerbank.answer%TYPE,
-                                          p_c_text answerbank.answer%TYPE,p_d_text answerbank.answer%TYPE,
-                                          p_a_count NUMBER,p_b_count NUMBER,p_c_count NUMBER,p_d_count NUMBER) IS
+        CLOSE regissub_cur;
+        
+        
+        --- Max Score each Chapter ---
+    FOR i IN 1..3 LOOP    
+        OPEN maxscorechap_cur(p_subcode);
+        LOOP
+        FETCH maxscorechap_cur INTO maxscorechap_rec;
+        EXIT WHEN maxscorechap_cur%NOTFOUND ;
+    ----Analysis answer----
+        IF maxscorechap_rec.answer = 'A' THEN
+            v_ans := maxscorechap_rec.a_id;
+            CASE
+                WHEN maxscorechap_rec.testanswer = 'A' THEN
+                    v_test := maxscorechap_rec.a_id;
+                WHEN maxscorechap_rec.testanswer = 'B' THEN
+                    v_test := maxscorechap_rec.b_id;
+                WHEN maxscorechap_rec.testanswer = 'C' THEN
+                    v_test := maxscorechap_rec.c_id;
+                ELSE
+                    v_test := maxscorechap_rec.c_id;
+            END CASE;
+        ELSIF regisid_rec.answer = 'B' THEN
+            v_ans := maxscorechap_rec.b_id;
+            CASE
+                WHEN maxscorechap_rec.testanswer = 'A' THEN
+                    v_test := regisid_rec.a_id;
+                WHEN maxscorechap_rec.testanswer = 'B' THEN
+                    v_test := regisid_rec.b_id;
+                WHEN maxscorechap_rec.testanswer = 'C' THEN
+                    v_test := maxscorechap_rec.c_id;
+                ELSE
+                    v_test := maxscorechap_rec.c_id;
+            END CASE;
+        ELSIF regisid_rec.answer = 'C' THEN
+            v_ans := maxscorechap_rec.c_id;
+            CASE
+                WHEN maxscorechap_rec.testanswer = 'A' THEN
+                    v_test := maxscorechap_rec.a_id;
+                WHEN maxscorechap_rec.testanswer = 'B' THEN
+                    v_test := maxscorechap_rec.b_id;
+                WHEN maxscorechap_rec.testanswer = 'C' THEN
+                    v_test := maxscorechap_rec.c_id;
+                ELSE
+                    v_test := maxscorechap_rec.c_id;
+            END CASE;
+        ELSE
+            v_ans := maxscorechap_rec.d_id;
+            CASE
+                WHEN maxscorechap_rec.testanswer = 'A' THEN
+                    v_test := maxscorechap_rec.a_id;
+                WHEN maxscorechap_rec.testanswer = 'B' THEN
+                    v_test := maxscorechap_rec.b_id;
+                WHEN maxscorechap_rec.testanswer = 'C' THEN
+                    v_test := maxscorechap_rec.c_id;
+                ELSE
+                    v_test := maxscorechap_rec.c_id;
+            END CASE;
+        END IF;
+        ----Count score----
+        IF maxscorechap_rec.answer = maxscorechap_rec.testanswer THEN 
+            v_correct_count_chap := v_correct_count_chap + 1;
+        END IF;
+        v_maxscorechap := v_maxscorechap + 1;
+        
+        END LOOP;
+        
+        DBMS_OUTPUT.PUT_LINE('Score of ' || regisid_rec.subjectcode || ' in chapter '|| v_chapter ||' is ' || v_correct_count_chap || ' / ' ||  v_maxscorechap);
+        CLOSE maxscorechap_cur;
+        v_chapter := v_chapter +1;
+        v_maxscorechap := 0;
+    END LOOP;    
+        
+        DBMS_OUTPUT.PUT_LINE('Score of ' || regisid_rec.subjectcode || ' is ' || v_correct_count || ' / ' ||  v_maxscore);
+        
+    END ANALYZE_TEST;
     
-    answer_unsort answer_table := answer_table();
-    answer_sort answer_table := answer_table();
-    v_count NUMBER := 0; 
-BEGIN
-    answer_unsort.EXTEND(4);
-    
-    answer_unsort(1) := answer_obj(p_a_count,p_a_text);
-    answer_unsort(2) := answer_obj(p_b_count,p_b_text);
-    answer_unsort(3) := answer_obj(p_c_count,p_c_text);
-    answer_unsort(4) := answer_obj(p_d_count,p_d_text);
-    
-    SELECT CAST(MULTISET(SELECT *
-                FROM TABLE(answer_unsort)
-                ORDER BY 1 DESC)AS answer_table)
-    INTO answer_sort
-    FROM DUAL;
-     v_count := answer_sort.COUNT();
-    FOR i IN 1..v_count LOOP
-        DBMS_OUTPUT.PUT_LINE(answer_sort(i).text_value || ' : ' || answer_sort(i).count_value);
-    END LOOP;
-END SORT_ANSWER;
-
-EXECUTE ANALYZE_TEST(300002);
+execute ANALYZE_TEST ('INT102');
+set serveroutput on;
